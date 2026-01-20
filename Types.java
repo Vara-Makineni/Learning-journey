656322f3c7dfe0c6412346357c0e236f15f3036f
vara <varasairammakineni@gmail.com>
2026-01-20 14:20:27 -0600

    new codes added

---
+++ Types.java
@@ -0,0 +1,16 @@
+public class Types {
+    public static void main(String[]args){
+
+        int a=10;
+        float b=10.5f;
+        byte c=25;
+        long d=1345;
+        short e=789;
+        double f=10.9998;
+
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f);
+
+       
+
+    }
+}
